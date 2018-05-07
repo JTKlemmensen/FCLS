4aee92f9171ec48795086c50bf34580ddd26baab
JTKlemmensen <25729544+JTKlemmensen@users.noreply.github.com>
2018-05-07 13:25:20 +0200

    implemented calculateRate method

--- src/logic/CarDataModel.java
+++ src/logic/CarDataModel.java
@@ -1,6 +1,25 @@
 package logic;
 
+import java.math.BigDecimal;
+
 public class CarDataModel
 {
-
+    private String name;
+    private BigDecimal limit;
+    
+    public CarDataModel(String name, BigDecimal limit)
+    {
+	this.name = name;
+	this.limit = limit;
+    }
+    
+    public String getName()
+    {
+	return name;
+    }
+    
+    public BigDecimal limit()
+    {
+	return limit;
+    }
 }
--- src/logic/LoanHandler.java
+++ src/logic/LoanHandler.java
@@ -1,20 +1,22 @@
 package logic;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Observable;
-
-
 import com.ferrari.finances.dk.rki.Rating;
 
-public class LoanHandler
+public class LoanHandler extends Observable
 {
-
     private LoanAgreementDataModel loanAgreement;
     private double dailyRate;
     private Rating rating;
     
     public LoanAgreementDataModel requestLoanAgreement(String carPrice, String downPayment, Date StartDate, String duration, CarDataModel car)
     {
+	double rate = calculateRate(dailyRate, rating, carPrice, downPayment, duration);
+	
+	
+	
 	return null;
     }
     
@@ -25,17 +27,31 @@ public class LoanHandler
     
     private double calculateRate(double dailyRate, Rating rating, String carPrice, String downPayment, String duration)
     {
-	return 0.0;
+	switch(rating)
+	{
+	case A: dailyRate+=1; break;
+	case B: dailyRate+=2; break;
+	default: dailyRate+=3; break;
+	}
+	
+	BigDecimal percentageOfCarPaid = new BigDecimal(downPayment).divide(new BigDecimal(carPrice));
+	
+	if(percentageOfCarPaid.compareTo(new BigDecimal(0.5))<0)
+	    dailyRate+=1;
+	    
+	if(Integer.parseInt(duration) > 3)
+	    dailyRate+=1;
+	    
+	return dailyRate;
     }
 
+    public void setRating(Rating rating) 
+    {
+	this.rating = rating;
+    }
 
-
-	public void setRating(Rating rating) {
-		this.rating = rating;
-	}
-
-	public void setRate(double rate) {
-		this.dailyRate = rate;
-	}
-
+    public void setRate(double rate) 
+    {
+	this.dailyRate = rate;
+    }
 }

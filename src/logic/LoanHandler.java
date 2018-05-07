1e1801a76d11cb724c03472041e6bfb55bd12713
JTKlemmensen <25729544+JTKlemmensen@users.noreply.github.com>
2018-05-07 11:44:47 +0200

    Added DataModel classes and methods for LoanHandler

--- src/logic/LoanHandler.java
+++ src/logic/LoanHandler.java
@@ -1,6 +1,28 @@
 package logic;
 
+import java.util.Date;
+import java.util.Observable;
+
+import com.ferrari.finances.dk.rki.Rating;
+
 public class LoanHandler
 {
-
+    private LoanAgreementDataModel loanAgreement;
+    private double dailyRate;
+    private Rating rating;
+    
+    public LoanAgreementDataModel requestLoanAgreement(String carPrice, String downPayment, Date StartDate, String duration, CarDataModel car)
+    {
+	return null;
+    }
+    
+    public void setupLoanAgreement(CustomerDataModel customer)
+    {
+	
+    }
+    
+    private void calculateRate(double dailyRate, Rating rating, String carPrice, String downPayment, String duration)
+    {
+	
+    }
 }

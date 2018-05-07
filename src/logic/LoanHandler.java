2d6ad4595f9c67d21962cca8cc86847c5454ddeb
Barbeos <32571022+Barbeos@users.noreply.github.com>
2018-05-07 12:11:03 +0200

    loangreement fix

--- src/logic/LoanHandler.java
+++ src/logic/LoanHandler.java
@@ -1,16 +1,14 @@
 package logic;
 
-<<<<<<< HEAD
 import java.util.Date;
 import java.util.Observable;
 
-=======
->>>>>>> 27dee8ac088cdf39406771266d8470f786d2b9eb
+
 import com.ferrari.finances.dk.rki.Rating;
 
 public class LoanHandler
 {
-<<<<<<< HEAD
+
     private LoanAgreementDataModel loanAgreement;
     private double dailyRate;
     private Rating rating;
@@ -29,17 +27,15 @@ public class LoanHandler
     {
 	return 0.0;
     }
-=======
-	private Rating rating;
-	private double rate;
+
+
 
 	public void setRating(Rating rating) {
 		this.rating = rating;
 	}
 
 	public void setRate(double rate) {
-		this.rate = rate;
+		this.dailyRate = rate;
 	}
 
->>>>>>> 27dee8ac088cdf39406771266d8470f786d2b9eb
 }

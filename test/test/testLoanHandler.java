573c68b6de833c6cbce60ab888ac0eb06f0e1e6c
Jonas Klemmensen <jonastklemmensen@gmail.com>
2018-05-16 23:09:04 +0200

    testRequestLoanAgreement -> interestrate

--- test/test/testLoanHandler.java
+++ test/test/testLoanHandler.java
@@ -6,6 +6,8 @@ import java.math.BigDecimal;
 
 import org.junit.Test;
 
+import com.ferrari.finances.dk.rki.Rating;
+
 import logic.CarDataModel;
 import logic.CustomerDataModel;
 import logic.LoanAgreementDataModel;
@@ -14,5 +16,18 @@ import logic.sellerDataModel;
 
 public class testLoanHandler {
 
+	@Test
+	public void testRequestLoanAgreement()
+	{
+		LoanHandler loanHandler = new LoanHandler();
+		loanHandler.setupLoanAgreement(new CustomerDataModel("", "", "", "", "", "", "", "1234567890"));
+		loanHandler.getLoanAgreementDataModel().setAskingPrice("10000000");
+		loanHandler.getLoanAgreementDataModel().setDownPayment( "2000000");
+		loanHandler.getLoanAgreementDataModel().setDuration("6");
+		loanHandler.setRating(Rating.A);
 
+		loanHandler.requestLoanAgreement(new sellerDataModel("", "", ""));
+		
+		assertEquals("3.0",loanHandler.getLoanAgreementDataModel().getInterestRate());
+	}
 }

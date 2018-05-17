6be6bfaa0a1328e3593f183f3b1ee335f3c456e1
Jonas Klemmensen <jonastklemmensen@gmail.com>
2018-05-17 14:31:57 +0200

    FindCustomer now retrieves customers from db

--- src/logic/CustomerHandler.java
+++ src/logic/CustomerHandler.java
@@ -9,23 +9,29 @@ public class CustomerHandler {
 
 	private List<CustomerDataModel> customers;
 	
+	CustomerDAO customerDAO;
+	
+	public CustomerHandler()
+	{
+		customerDAO = new CustomerDAO();
+	}
+	
 	public boolean insertToDB(CustomerDataModel customer) {
-		CustomerDAO CD = new CustomerDAO();
-		return CD.createNewCustomer(customer);	
+		return customerDAO.createNewCustomer(customer);	
 	}
 	
 	public List<CustomerDataModel> getCustomers(String firstName, String lastName, String phoneNumber)
 	{
-//		if(customers==null)
-//			customers = CustomerDAO.getAll();
+		if(customers==null)
+			customers = customerDAO.getCustomerList();
 		
-		customers = new ArrayList<CustomerDataModel>();
-		customers.add(new CustomerDataModel("Peter", "Jensen", "Villavej 25", "Herning", "7400", "12645678", "ja", "yes"));
-		customers.add(new CustomerDataModel("Ole", "Jensen", "Villavej 24", "Herning", "7400", "12342378", "ja", "yes"));
-		customers.add(new CustomerDataModel("Niels", "Jensen", "Villavej 23", "Herning", "7400", "76531247", "ja", "yes"));
-		customers.add(new CustomerDataModel("Henrik", "Jensen", "Villavej 21", "Herning", "7400", "90843231", "ja", "yes"));
-		customers.add(new CustomerDataModel("Per", "Jensen", "Villavej 22", "Herning", "7400","97423231", "PerJensen@hot.com", "1234567790"));
-		
+//		customers = new ArrayList<CustomerDataModel>();
+//		customers.add(new CustomerDataModel("Peter", "Jensen", "Villavej 25", "Herning", "7400", "12645678", "ja", "yes"));
+//		customers.add(new CustomerDataModel("Ole", "Jensen", "Villavej 24", "Herning", "7400", "12342378", "ja", "yes"));
+//		customers.add(new CustomerDataModel("Niels", "Jensen", "Villavej 23", "Herning", "7400", "76531247", "ja", "yes"));
+//		customers.add(new CustomerDataModel("Henrik", "Jensen", "Villavej 21", "Herning", "7400", "90843231", "ja", "yes"));
+//		customers.add(new CustomerDataModel("Per", "Jensen", "Villavej 22", "Herning", "7400","97423231", "PerJensen@hot.com", "1234567790"));
+//		
 		List<CustomerDataModel> customers = new ArrayList<CustomerDataModel>();
 		
 		for(CustomerDataModel cdm : this.customers)

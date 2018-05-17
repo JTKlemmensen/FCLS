ac6e41b5fb41ac2480a8de06a79850388def0757
Barbeos <32571022+Barbeos@users.noreply.github.com>
2018-05-17 08:34:22 +0200

    double tetsloanagreementdatamodel

--- test/test/TestLoanAgreementDataModel.java
+++ test/test/TestLoanAgreementDataModel.java
@@ -18,11 +18,24 @@ public class TestLoanAgreementDataModel {
 	public void testCustomer() {
 		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
 		assertEquals(customer, lGDM.getCustomer());
+	}	
+	@Test
+	public void testCustomer2() {
+		CustomerDataModel customer2 = new CustomerDataModel("Ben", "Nielsen", "langeløgallé 53", "Lem", "7680", "45252343", "Submi@hotmail.com", "0102036434");
+		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer2);
+		assertEquals(customer2, lGDM.getCustomer());
 	}
 	@Test
 	public void testSeller() {
 		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
 		sellerDataModel seller = new sellerDataModel("heiter", "500000", "Jacob Ingilesen");
+		lGDM.setSeller(seller);
+		assertEquals(seller, lGDM.getSeller());
+	}
+	@Test
+	public void testSeller2() {
+		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
+		sellerDataModel seller = new sellerDataModel("Bennie", "600000", "Ben Nielsen");
 		lGDM.setSeller(seller);
 		assertEquals(seller, lGDM.getSeller());
 	}
@@ -34,21 +47,49 @@ public class TestLoanAgreementDataModel {
 		assertEquals(car, lGDM.getCar());
 	}
 	@Test
+	public void testCar2() {
+		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
+		CarDataModel car = new CarDataModel("f14", new BigDecimal(3000000), "2 døre");
+		lGDM.setCar(car);
+		assertEquals(car, lGDM.getCar());
+	}
+	@Test
 	public void testIsApproved() {
 		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
 		lGDM.setApproved(true);
 		assertEquals(true, lGDM.isApproved());
 	}
 	@Test
+	public void testIsApproved2() {
+		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
+		lGDM.setApproved(false);
+		assertEquals(false, lGDM.isApproved());
+	}
+	@Test
 	public void testDuration() {
 		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
-		lGDM.setDuration("20");
-		assertEquals("20", lGDM.getDuration());
+		String duration = "20";
+		lGDM.setDuration(duration);
+		assertEquals(duration, lGDM.getDuration());
+	}
+	@Test
+	public void testDuration2() {
+		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
+		String duration = "30";
+		lGDM.setDuration(duration);
+		assertEquals(duration, lGDM.getDuration());
 	}
 	@Test
 	public void testAskingPrice() {
 		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
 		String askingPrice = "2000000";
+		lGDM.setAskingPrice(askingPrice);
+		assertEquals(askingPrice, lGDM.getAskingPrice());
+	}
+	@Test
+	public void testAskingPrice2() {
+		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
+		String askingPrice = "3000000";
 		lGDM.setAskingPrice(askingPrice);
 		assertEquals(askingPrice, lGDM.getAskingPrice());
 	}
@@ -67,11 +108,24 @@ public class TestLoanAgreementDataModel {
 		assertEquals(interestRate, lGDM.getInterestRate());
 	}
 	@Test
+	public void testInterestRate2() {
+		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
+		String interestRate = "4";
+		lGDM.setInterestRate(interestRate);
+		assertEquals(interestRate, lGDM.getInterestRate());
+	}
+	@Test
 	public void testLocalDate() {
 		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
 		LocalDate localDate = LocalDate.of(2019, 10, 12);
 		lGDM.setStartDate(localDate);
 		assertEquals(localDate, lGDM.getStartDate());
 	}
-
+	@Test
+	public void testLocalDate2() {
+		LoanAgreementDataModel lGDM = new LoanAgreementDataModel(customer);
+		LocalDate localDate = LocalDate.of(2020, 11, 9);
+		lGDM.setStartDate(localDate);
+		assertEquals(localDate, lGDM.getStartDate());
+	}
 }

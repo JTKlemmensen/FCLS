d698ef57894870cf6a078573c91eea93c87b6aca
Jonas Klemmensen <jonastklemmensen@gmail.com>
2018-05-18 14:47:36 +0200

    Now uses Alert instead of creating our own Window

--- src/logic/RKIandBank.java
+++ src/logic/RKIandBank.java
@@ -5,7 +5,14 @@ import com.ferrari.finances.dk.rki.CreditRator;
 import com.ferrari.finances.dk.rki.Rating;
 
 import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.image.Image;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
 import view.BadCreditScreenController;
+import view.FCLSController;
 
 
 public class RKIandBank extends Thread
@@ -21,7 +28,7 @@ public class RKIandBank extends Thread
 	public void run(){	
 		Rating rating = CreditRator.i().rate(this.CPR);
 		this.loanHandler.setRating(rating);
-		
+
 		if(Rating.D != rating)
 		{
 			this.loanHandler.setRate((InterestRate.i().todaysRate()));
@@ -40,11 +47,11 @@ public class RKIandBank extends Thread
 			Platform.runLater(new Runnable() {
 				public void run() 
 				{
-				//TODO 
-				//gui is a part of logic?! - also replace with javabuiltin warningscreens perhaps
-				BadCreditScreenController controller = new BadCreditScreenController();
-				controller.showBadCreditScreen();
-			}
+				Alert alert = new Alert(AlertType.NONE,"Kunde er registreret som dårlig betaler, lånetilbud er afvist",new ButtonType("Accepter"));
+				alert.setTitle("RKI Afvisning");
+				alert.showAndWait();
+				FCLSController.INSTANCE.changeView(null);
+				}
 			});
 		}
 	}
